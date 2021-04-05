c12a90eb0b12ab7cf5c70f6a78fc712a369f1c88
Jonathan Castillo <jonacastw@gmail.com>
2021-04-05 12:54:13 -0500

    historial-resumen

--- src/models/FilesRemmaq.js
+++ src/models/FilesRemmaq.js
@@ -2,11 +2,13 @@ const mongoose = require('mongoose');
 
 //------------ Files Remmaq Schema ------------//
 const FileRemmaqSchema = new mongoose.Schema({
-    titu: { type: String},
-    origen: { type: String},
+    tituloArchivo: { type: String},
+    origen: { type: String,default: 'REMMAQ'},
     magnitud: { type: String},
-    description: { type: String},
-    date:{type:Date,default:Date.now}
+    description: { type: String,default:'Sin descripcion'},
+    nombreestaciones:{type: String},
+    date:{type:Date,default:Date.now},
+    user:{type: String}
     });
 
 const FileRemmaq = mongoose.model('FileRemmaq', FileRemmaqSchema);
