789ce1e75076167d2bc95d5e21aad9faa7fdbbea
Jonathan Castillo <jonacastw@gmail.com>
2021-05-03 01:45:07 -0500

    upload inhami - save dates remmaq

--- src/models/FilesRemmaq.js
+++ src/models/FilesRemmaq.js
@@ -3,14 +3,17 @@ const mongoose = require('mongoose');
 //------------ Files Remmaq Schema ------------//
 const FileRemmaqSchema = new mongoose.Schema({
     tituloArchivo: { type: String},
-    origen: { type: String,default: 'REMMAQ'},
+    origen: { type: String,default:"REMMAQ"},
     magnitud: { type: String},
     description: { type: String,default:'Sin descripcion'},
     nombreestaciones:{type: String},
     date:{type:Date,default:Date.now},
+    fechainicio:{type:String},
+    fechafin:{type:String},
+    path:{type: String},
     user:{type: String}
     });
 
-const FileRemmaq = mongoose.model('FileRemmaq', FileRemmaqSchema);
+const FileRemmaq = mongoose.model('encabezado', FileRemmaqSchema);
 
 module.exports = FileRemmaq;
