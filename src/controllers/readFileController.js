34369a9851863db98fde684f9f005aae8e9b7eea
Jonathan Castillo <jonacastw@gmail.com>
2021-05-16 19:27:05 -0500

    save inamhi data

--- src/controllers/readFileController.js
+++ src/controllers/readFileController.js
@@ -1,6 +1,9 @@
 const FileModel = require("../models/fileModel");
 const readline = require('readline');
 const fs = require('fs');
+
+const Encabezado = require("../models/encabezado");
+const Datos = require("../models/datos");
 //Delimitadores
 const CODIGO_FIN_DELIMITADOR = 'CODIGO';
 const CODIGO_INICIO_DELIMITADOR = ':';
@@ -9,24 +12,24 @@ class ReadFileController {
     }
 
 
-    ReadTxtFile(req, cb) {
+    ReadTxtFile(filePath, cb) {
         let result = new FileModel();
-        this.ReadContentTxtFile(req, file => {
+        this.ReadContentTxtFile(filePath, file => {
             result = file;
             // console.log(result);
             cb(result);
         })
     }
 
-    ReadContentTxtFile(req, process) {
+    ReadContentTxtFile(filePath, process) {
         let file = new FileModel();
-        fs.readFile(`${req.file.path}`, 'utf8', function (err, data) {
+        fs.readFile(`${filePath}`, 'utf8', function (err, data) {
             if (err) {
                 console.log(err);
             } else {
                 // console.log(typeof (data));
                 let lector = readline.createInterface({
-                    input: fs.createReadStream(`${req.file.path}`)
+                    input: fs.createReadStream(`${filePath}`)
                 });
 
                 let i = 0;
@@ -37,7 +40,6 @@ class ReadFileController {
                     if (i == 2) {
                         const index = linea.indexOf(")", 0);
                         file.magnitud = linea.substring(0, index + 1);
-                        // console.log(file.magnitud);
                     }
 
                     //nombre estacion
@@ -45,17 +47,14 @@ class ReadFileController {
                         const startIndex = linea.indexOf(CODIGO_INICIO_DELIMITADOR, 0);
                         const endIndex = linea.indexOf(CODIGO_FIN_DELIMITADOR, 0);
                         file.nombreEstaciones = linea.substring(startIndex + 1, endIndex).trim();
-                        // console.log(file.nombreEstaciones);
-                        // console.log(startIndex);
-                        // console.log(endIndex);
                     }
+
                     //Lectura de registros
                     if (i >= 11) {
                         const fila = linea.split(/\s+/);
                         const fechaFila = fila[0] + "/" + fila[1].padStart(2, '0');
-                        // console.log(fechaFila);
                         for (let index = 2; index < fila.length; index++) {
-                            const dia = "" + (index-1);
+                            const dia = "" + (index - 1);
                             const arreglo = {
                                 fecha: fechaFila + "/" + dia.padStart(2, '0'),
                                 pertenece: file.nombreEstaciones,
@@ -65,9 +64,6 @@ class ReadFileController {
                         }
 
                     }
-
-                    // const magnitud = linea.trim().split("\t");
-                    // console.log(magnitud[0])
                     i++;
                 });
 
@@ -76,10 +72,50 @@ class ReadFileController {
                     file.lecturas = registros;
                     file.fechaInicio = registros[0].fecha;
                     file.fechafin = registros[registros.length - 1].fecha;
+                    file.path = filePath;
                     console.log(file.fechaInicio)
                     process(file)
                 });
             }
+        });
+    }
+
+    async SaveFile(file) {
+        // const { tituloArchivo, origen, magnitud, description } = req.body;
+
+        let encabezado = new Encabezado();
+        encabezado.tituloArchivo = file.tituloArchivo;
+        encabezado.origen = file.origen;
+        encabezado.magnitud = file.magnitud;
+        encabezado.description = file.description;
+        
+
+        encabezado.user = file.userId;
+        encabezado.tituloArchivo = file.tituloArchivo;
+        encabezado.nombreestaciones = file.nombreEstaciones;
+        encabezado.fechainicio = file.fechaInicio;
+        encabezado.fechafin = file.fechafin;
+        encabezado.path = file.path;
+        await encabezado.save(function (error, room) {
+            console.log(room.id);
+            const data = [];
+            for (let j = 0; j < file.lecturas.length; j++) {
+                if (!isNaN(file.lecturas[j].valor)) {
+                    const dato = {
+                        encabezado: room.id,
+                        fecha: file.lecturas[j].fecha,
+                        pertenece: file.lecturas[j].pertenece,
+                        valor: file.lecturas[j].valor,
+                    };
+                    data.push(dato);
+                }
+            }
+
+            Datos.insertMany(data).then(function () {
+                console.log("Data inserted")  // Success
+            }).catch(function (error) {
+                console.log(error)      // Failure
+            });
         });
     }
 }
--- src/routes/loadInamhiFile.js
+++ src/routes/loadInamhiFile.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const router = express.Router();
+var sessionStorage = require('sessionstorage');
+
 const fileController = require("../controllers/readFileController");
+const Datos = require("../models/datos");
+const { Session } = require("express-session");
 
 function isAuthenticated(req, res, next) {
     if (req.isAuthenticated()) {
@@ -17,18 +21,29 @@ router.get("/users/uploadin", isAuthenticated, (req, res) => {
 router.post('/users/uploadin', isAuthenticated, (req, res) => {
 
     const fileControllerInstance = new fileController();
-    fileControllerInstance.ReadTxtFile(req, file => {
-        console.log("estructura generada");
-        // console.log(file);
-        // sessionStorage.setItem('fileGenerated',file);
-        console.log('qweq');
-        req.body.magnitud = file.magnitud;
-        console.log(req.body);
-        req.body.estacionesname=file.nombreEstaciones;
-        req.body.numregistros=file.lecturas.length;
-        req.body.firstdate=file.fechaInicio;
-        req.body.lastdate=file.fechafin;
-        res.render('users/resumentabladatos.hbs',{datosResumen: req.body})
+    fileControllerInstance.ReadTxtFile(req.file.path, file => {
+        req.body.estacionesname = file.nombreEstaciones;
+        req.body.numregistros = file.lecturas.length;
+        req.body.firstdate = file.fechaInicio;
+        req.body.lastdate = file.fechafin;
+        req.body.path = file.path;
+        req.body.userid = req.user.id;
+        res.render('users/resumentabladatos.hbs', { datosResumen: req.body })
+    });
+
+});
+router.post('/users/saveinamhi', isAuthenticated, async (req, res, next) => {
+    const fileControllerInstance = new fileController();
+    fileControllerInstance.ReadTxtFile(req.body.path, file => {
+        file.userId = req.body.userid;
+        req.body.estacionesname = file.nombreEstaciones;
+        req.body.numregistros = file.lecturas.length;
+        req.body.firstdate = file.fechaInicio;
+        req.body.lastdate = file.fechafin;
+        file.tituloArchivo = req.body.tituloArchivo;
+        file.descripcion = req.body.description;
+         fileControllerInstance.SaveFile(file);
+        res.render("users/historialArchivos.hbs", { datosResumen: req.body });
     });
 });
 
